fea8b26e795cf74c9833a8737bcded3f2145311d
saraelallouche <saraelallouche@hotmail.fr>
2023-06-28 15:30:50 +0200

    delete some files

--- spring-data-rest-quickstart/src/main/java/org/acme/spring/data/rest/FruitsRepository.java
+++
@@ -1,6 +0,0 @@
-package org.acme.spring.data.rest;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface FruitsRepository extends PagingAndSortingRepository<Fruit, Long> {
-}
